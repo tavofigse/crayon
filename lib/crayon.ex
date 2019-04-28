0304c16b6978e6d9b5c3026750bfdff19504159a
Gustavo <gfasequeira@gmail.com>
2019-04-28 00:17:54 -0300

    API change and adding some tests.

--- lib/crayon.ex
+++ lib/crayon.ex
@@ -1,29 +1,57 @@
 defmodule Crayon do
   @moduledoc """
-  Terminal string styling done right
+  Provides a simple API to change text color and styling
+  strings in the terminal.
   """
-  @colors [:black, :red, :green, :yellow, :blue, :magenta, :cyan, :white,
-           :light_black, :light_red, :light_green, :light_yellow, :light_blue,
-           :light_magenta, :light_cyan, :light_white]
+  alias IO.ANSI
 
-  @modifiers [:reset, :bright, :italic, :underline, :inverse, :framed]
-
-  defp sequence(escapes, string) do
-      escapes ++ [string, :reset]
-      |> IO.ANSI.format_fragment(true)
+  defp to_ansi(escape) do
+    case escape do
+      :black -> ANSI.black
+      :red -> ANSI.red
+      :green -> ANSI.green
+      :yellow -> ANSI.yellow
+      :blue -> ANSI.blue
+      :magenta -> ANSI.magenta
+      :cyan -> ANSI.cyan
+      :white -> ANSI.white
+      :lt_black -> ANSI.light_black
+      :lt_red -> ANSI.light_red
+      :lt_green -> ANSI.light_green
+      :lt_yellow -> ANSI.light_yellow
+      :lt_blue -> ANSI.light_blue
+      :lt_magenta -> ANSI.light_magenta
+      :lt_cyan -> ANSI.light_cyan
+      :lt_white -> ANSI.light_white
+      :lt_bg_black -> ANSI.light_black_background
+      :lt_bg_red -> ANSI.light_red_background
+      :lt_bg_green -> ANSI.light_green_background
+      :lt_bg_yellow -> ANSI.light_yellow_background
+      :lt_bg_blue -> ANSI.light_blue_background
+      :lt_bg_magenta -> ANSI.light_magenta_background
+      :lt_bg_cyan -> ANSI.light_cyan_background
+      :lt_bg_white -> ANSI.light_white_background
+      :bg_black -> ANSI.black_background
+      :bg_red -> ANSI.red_background
+      :bg_green -> ANSI.green_background
+      :bg_yellow -> ANSI.yellow_background
+      :bg_blue -> ANSI.blue_background
+      :bg_magenta -> ANSI.magenta_background
+      :bg_cyan -> ANSI.cyan_background
+      :bg_white -> ANSI.white_background
+      :bold -> ANSI.bright
+      :reset -> ANSI.reset
+      :italic -> ANSI.italic
+      :underline -> ANSI.underline
+      :inverse -> ANSI.inverse
+      _ -> escape
+    end
   end
 
-  @colors
-  |> Enum.each(fn (color) ->
-    def unquote(color)(string, opts \\ []) do
-      sequence([unquote(color)] ++ opts, string)
-    end
-  end)
+  def draw(escapes) do
+    escapes
+    |> Enum.map(fn (escape) -> to_ansi(escape) end)
+    |> IO.ANSI.format_fragment
+  end
 
-  @modifiers
-  |> Enum.each(fn (modifier) ->
-    def unquote(modifier)(string, opts \\ []) do
-      sequence([unquote(modifier)] ++ opts, string)
-    end
-  end)
 end
--- test/crayon_test.exs
+++ test/crayon_test.exs
@@ -2,7 +2,33 @@ defmodule CrayonTest do
   use ExUnit.Case
   doctest Crayon
 
-  test "greets the world" do
-    assert Crayon.hello() == :world
+  test "Test the green background with white color and bold strings" do
+    assert Crayon.draw([:bg_green, :white, :bold, "Hello, World! "]) == [[[[[], "\e[42m"], "\e[37m"], "\e[1m"], "Hello, World! "]
+  end
+
+  test "Test the simple red strings" do
+    assert Crayon.draw([:red, "Hello, World! "]) == [[[], "\e[31m"], "Hello, World! "]
+  end
+
+  test "Test the complex composition" do
+    assert Crayon.draw([
+      :green,
+      "I am a green line ",
+      :blue,
+      :underline,
+      :bold,
+      "With a blue substring",
+      :green,
+      " that become green again!"
+    ]) == [
+        [
+          [
+            [[[[[[], "\e[32m"], "I am a green line "], "\e[34m"], "\e[4m"], "\e[1m"],
+            "With a blue substring"
+          ],
+          "\e[32m"
+        ],
+        " that become green again!"
+      ]
   end
 end
